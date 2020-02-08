62c8c5599ddc15cac7a1efcea06a111cdf54894e
Japneet Singh <jpntsingh8@gmail.com>
2020-02-08 15:33:42 -0500

    script

--- backend/bundle.js
+++ backend/bundle.js
@@ -26,7 +26,9 @@ fetch('https://mavric.bid', {
   })
 })
 .then(resp => resp.json())
-.then(console.log)
+.then(({data}) => 
+  document.querySelector(".about-text").innerHTML = data.about.desc
+)
 
 // POST for about queries
 
