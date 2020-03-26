08400b6756cc66de842f5f7036f210f9f8133fb5
Japneet Singh <jpntsingh8@gmail.com>
2020-03-26 02:08:06 -0500

    resources changes

--- build.js
+++ build.js
@@ -3861,6 +3861,105 @@ let ev = document.querySelector(".ev")
 let about = document.querySelector(".about")
 let event = document.querySelector(".events")
 
+let date = new Date()
+let time = date.getHours()
+console.log(time)
+let theme = "light"
+if(time > 18 || time < 6 ) 
+    theme = "dark"
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFwbmVldCIsImEiOiJjazQzM25majcwM3diM21uMXk4bnVieWl6In0.Nw00DmDETPkUH7dtCSzC9Q';
+const map = new mapboxgl.Map({
+container: 'map', // container id
+style: `mapbox://styles/mapbox/${theme}-v10`, //hosted style id
+center: [-76.937759, 38.989697], // starting position
+zoom: 14 // starting zoom
+});
+
+var geojson = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [-76.9365, 38.9891]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'Iribe Center',
+        name: "iribe"
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [-76.9476, 38.9851]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'Brandon Morse',
+        name: 'bm'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [-76.9365, 38.9901]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'UMIACS',
+        name: "umiacs"
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [-76.9416, 38.9881]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'MITH',
+        name: 'mith'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [-76.9416, 38.9885]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'HCIL',
+        name: 'HCIL'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [-76.9365, 38.9895]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'Diversity and Inclusion in Computing',
+        name: "dic"
+        }
+    }
+]
+};
+geojson.features.forEach((marker) => {
+    let el = document.createElement('div')
+    el.className = "marker"
+    el.style.backgroundImage = `url(./assets/${marker.properties.name}.png)`
+    new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map)
+})
+
+
+
 ab.addEventListener("click", () => {
   jump(about)
 })
@@ -3917,6 +4016,33 @@ fetch('https://mavric.bid', {
           num+=1
         i+=1
     })
+    let mapify = (e) => {
+      console.log("hi")
+      let locate = e.target.innerHTML
+      for(let loc of geojson.features) {
+        console.log(loc.properties.description)
+        console.log(locate)
+        console.log("hi" === "hi")
+        console.log(`${loc.properties.description}`.toString() === `${locate}`.toString())
+        if(`${loc.properties.description}` == `${locate}`) {
+          let x = loc.geometry.coordinates[0]
+          let y = loc.geometry.coordinates[1]
+          map.flyTo({
+            center: [
+              x,y
+            ],
+            essential: true,
+            zoom: 17
+          })
+        }
+      }  
+    }
+    let names = document.querySelectorAll(".resource-p")
+    console.log(names)
+    for (let p of names) {
+      console.log(p)
+      p.addEventListener("click",mapify)
+    }
 }) 
   
 let data = {
@@ -3969,4 +4095,5 @@ let change = () => {
 document.querySelector(".glide__arrow").addEventListener("click", () => {
   setInterval(change, 1000)
 })
+
 },{"@glidejs/glide":1,"jump.js":2}]},{},[3]);
